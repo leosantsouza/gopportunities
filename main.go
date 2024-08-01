6aaa323cf373adbfdac86796174ac0c2067ccb7a
leosantsouza <leosantsouza@hotmail.com>
2024-08-01 17:09:44 -0300

    feat: Create config

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"github.com/leosantsouza/gopportunities.git/config"
 	"github.com/leosantsouza/gopportunities.git/router"
 )
 
 func main() {
   
+	// Initialize Configs
+	err  := config.Init()
+	if err != nil{
+		println(err)
+		return
+	}
+
   // Initialize router
 	router.Initialize()
 
