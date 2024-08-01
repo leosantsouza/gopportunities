9f3555fc3daa02d1e0d5491d4c83c3a55a183d24
leosantsouza <leosantsouza@hotmail.com>
2024-08-01 16:33:34 -0300

    feat: update handler structute to separate package

--- router/router.go
+++ router/router.go
@@ -4,12 +4,13 @@ import "github.com/gin-gonic/gin"
 
 func Initialize() {
 
+//Initializer Router
 	router := gin.Default()
 	
-// Initialize routes
+// Initialize Routes
 	initializeRoutes(router)
 
-
+// Run the Server
 	router.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 
 }
